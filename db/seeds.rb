c1c1cc1d7276e129aba0b6e81d9e55c91b65864e
Shriyam Singh <singh.shriyam@gmail.com>
2024-11-05 12:26:46 +0100

    index3

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,6 +12,8 @@ user1 = User.create!(
   age: 25,
   description: "First user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user2 = User.create!(
   email: "user2@example.com",
@@ -21,6 +23,8 @@ user2 = User.create!(
   age: 30,
   description: "Second user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user3 = User.create!(
   email: "user3@example.com",
@@ -30,6 +34,8 @@ user3 = User.create!(
   age: 35,
   description: "Third user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user3.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user4 = User.create!(
   email: "user4@example.com",
@@ -39,6 +45,8 @@ user4 = User.create!(
   age: 35,
   description: "Fourth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user4.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user5 = User.create!(
   email: "user5@example.com",
@@ -48,6 +56,8 @@ user5 = User.create!(
   age: 27,
   description: "Fifth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user5.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user6 = User.create!(
   email: "user6@example.com",
@@ -57,6 +67,8 @@ user6 = User.create!(
   age: 29,
   description: "Sixth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user6.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user7 = User.create!(
   email: "user7@example.com",
@@ -66,6 +78,8 @@ user7 = User.create!(
   age: 31,
   description: "Seventh user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user7.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user8 = User.create!(
   email: "user8@example.com",
@@ -75,6 +89,8 @@ user8 = User.create!(
   age: 26,
   description: "Eighth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user8.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user9 = User.create!(
   email: "user9@example.com",
@@ -84,6 +100,8 @@ user9 = User.create!(
   age: 33,
   description: "Ninth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user9.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 user10 = User.create!(
   email: "user10@example.com",
@@ -93,6 +111,8 @@ user10 = User.create!(
   age: 34,
   description: "Tenth user example description."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+user10.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 puts "Creating locals..."
 local1 = Local.create!(
@@ -106,6 +126,8 @@ local1 = Local.create!(
   user_id: user1.id,
   description: "Guide with a passion for history and cuisine in Berlin."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689977968861-9c91dbb16049?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local1.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local2 = Local.create!(
   first_name: "Judit",
@@ -118,6 +140,8 @@ local2 = Local.create!(
   user_id: user2.id,
   description: "Art enthusiast with a knowledge of Munich's culture and scenery."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1688572454849-4348982edf7d?q=80&w=2576&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local2.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local3 = Local.create!(
   first_name: "Aditi",
@@ -130,6 +154,8 @@ local3 = Local.create!(
   user_id: user3.id,
   description: "Architect with knowledge on Cologne's top historic sites."
 )
+file = URI.parse("https://images.unsplash.com/photo-1667053508464-eb11b394df83?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTV8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww").open
+local3.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local4 = Local.create!(
   first_name: "Joel",
@@ -142,6 +168,8 @@ local4 = Local.create!(
   user_id: user4.id,
   description: "Professional with a knack for finding the best shopping spots."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=2671&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local4.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local5 = Local.create!(
   first_name: "Carlos",
@@ -154,6 +182,8 @@ local5 = Local.create!(
   user_id: user5.id,
   description: "Foodie with an insider's guide to Hamburg's culinary delights."
 )
+# file = URI.parse("https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+# local5.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local6 = Local.create!(
   first_name: "Emma",
@@ -166,11 +196,13 @@ local6 = Local.create!(
   user_id: user6.id,
   description: "Art lover familiar with Stuttgart's top tech spots and galleries."
 )
+file = URI.parse("https://images.unsplash.com/photo-1702482527875-e16d07f0d91b?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local6.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local7 = Local.create!(
-  first_name: "Hiro",
-  last_name: "Tanaka",
-  name: "Hiro Tanaka",
+  first_name: "Hera",
+  last_name: "Thakur",
+  name: "Hera Thakur",
   languages: "Japanese, English",
   city: "Düsseldorf",
   categories: "Culture, Food",
@@ -178,6 +210,8 @@ local7 = Local.create!(
   user_id: user7.id,
   description: "Specializing in Japanese cuisine and culture in Düsseldorf."
 )
+file = URI.parse("https://plus.unsplash.com/premium_photo-1689629870780-5d0e655383e6?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local7.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local8 = Local.create!(
   first_name: "Lena",
@@ -190,6 +224,8 @@ local8 = Local.create!(
   user_id: user8.id,
   description: "Deep knowledge of Nuremberg's festivals and historic landmarks."
 )
+file = URI.parse("https://images.unsplash.com/photo-1653379671625-35943f524abc?q=80&w=2669&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local8.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local9 = Local.create!(
   first_name: "Ahmed",
@@ -202,6 +238,8 @@ local9 = Local.create!(
   user_id: user9.id,
   description: "Showing the best of Bonn's natural and scientific spots."
 )
+file = URI.parse("https://images.unsplash.com/photo-1724159768353-55b083d0d435?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzh8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww").open
+local9.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 local10 = Local.create!(
   first_name: "Elena",
@@ -214,6 +252,8 @@ local10 = Local.create!(
   user_id: user10.id,
   description: "Familiar with Leipzig's literary heritage and art scene."
 )
+file = URI.parse("https://images.unsplash.com/photo-1632324343640-86af9827dbeb?q=80&w=2574&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").open
+local10.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
 
 puts "Creating bookings..."
 Booking.create!(
