b3fff023aeafa97173a286c08d846fb1ad551f18
Joel Armenta <armentamj@gmail.com>
2024-11-06 17:43:55 +0100

    search route and all function and almost complete view.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,12 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :styles]
+  skip_before_action :authenticate_user!, only: [:home, :styles, :search]
 
   def home
   end
 
   def styles
+  end
+
+  def search
   end
 end
