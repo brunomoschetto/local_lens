252f24ff0afb93b5463e3c5b41b1231e35ef37fc
Shriyam Singh <singh.shriyam@gmail.com>
2024-11-06 17:09:06 +0100

    index4

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
     resources :bookings, only: [:new, :create]
   end
 
-  resources :bookings, only: [:index]
+  resources :bookings, only: [:index, :edit, :update, :destroy]
 end
