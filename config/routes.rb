164e72f04ec355aa614021242b3877e95e88a385
Shriyam Singh <singh.shriyam@gmail.com>
2024-11-11 11:51:01 +0100

    navbar2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'bookings/index'
   get '/styles', to: 'pages#styles'
+  get '/profile', to: 'pages#profile'
   get '/search', to: 'pages#search', as: :search
   devise_for :users
   root to: "pages#home"
