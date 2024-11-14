7c842438b96fd29d8837434ff8e1d2871aa6f547
Shriyam Singh <singh.shriyam@gmail.com>
2024-11-14 12:17:28 +0100

    delete

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
-  has_many :bookings
-  has_one :local
+  has_many :bookings, dependent: :destroy
+  has_one :local, dependent: :destroy
   has_one_attached :photo
   has_many :reviews, dependent: :destroy
 
