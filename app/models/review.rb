57d37ccad1935c7bc863d1ec0241654c0f0287ce
Bruno Moschetto <bruno.moschetto@gmail.com>
2024-12-05 16:00:38 +0100

    some search and button final adjustments

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
   belongs_to :local
   belongs_to :user
 
-  validates :title, presence: true, length: { minimum: 10 }
-  validates :comment, presence: true, length: { minimum: 20 }
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :comment, presence: true, length: { minimum: 5 }
   validates :rating, presence: true, numericality: { only_integer: true, in: 1..5 }
 end
